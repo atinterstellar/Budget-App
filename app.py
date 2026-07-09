fc2f154e5a0a1f8199fcbfd2ff5f636f3f2b1933
Atin Kumar Chakraborty <atinkumarchakraborty@Atins-MacBook-Air.local>
2026-07-09 21:31:28 +0530

    Added Withdraw and Get Balance

--- app.py
+++ app.py
@@ -41,6 +41,37 @@ def deposit_cat():
         return redirect(url_for('cat_ledger', name=name))
     return render_template('cat_deposit.html' , chosen = chosen)
 
+@app.route('/transaction_from', methods = ['GET','POST'])
+def transaction_from():
+    if request.method == 'POST':
+        chosen = request.form.get("category") 
+        return redirect(url_for('withdraw_cat', name = chosen))
+    return render_template('transaction_from.html', categories = main_app.categories)
 
+@app.route('/withdraw_cat', methods = ['GET','POST'])
+def withdraw_cat():
+    name = request.args.get("name")
+    chosen = main_app.get_category(name)
+    if request.method == 'POST':
+        amount = request.form.get("amount")
+        desc = request.form.get("desc")
+        chosen.withdraw(float(amount), desc)
+        return redirect(url_for('cat_ledger', name=name))
+    return render_template('withdraw_cat.html' , chosen = chosen)
+
+@app.route('/bal', methods = ['GET','POST'])
+def bal():
+    if request.method == 'POST':
+        chosen = request.form.get("category") 
+        return redirect(url_for('bal_cat', name = chosen))
+    return render_template('bal.html', categories = main_app.categories)
+
+@app.route('/bal_cat', methods = ['GET','POST'])
+def bal_cat():
+    name = request.args.get("name")
+    chosen = main_app.get_category(name)
+    balance = chosen.get_balance()
+    return render_template('bal_cat.html' , balance = balance, chosen = chosen)
+    
 if __name__ == '__main__' :
     app.run(host = '0.0.0.0' , port = 5101 , debug = True)
